4d4a7fad8a7e05fad5ee62a22182813a5430f90d
Gabor Barna <gbarna@whitepages.com>
2016-09-07 15:49:37 +0200

    rename `caller_identification` endpoint to phone search

--- test/code_examples/core_test.clj
+++ test/code_examples/core_test.clj
@@ -2,12 +2,12 @@
   (:require [clojure.test :refer :all]
             [code-examples.identity-check :as idc]
             [code-examples.lead-verify :as lv]
-            [code-examples.caller-identification :as cid]))
+            [code-examples.phone-search :as ps]))
 
 (deftest a-test
   (testing "Identity check shall return a coll with 8 items"
     (is (= 8 (count (idc/run-request)))))
   (testing "Lead verify shall return a coll with 5 items"
     (is (= 5 (count (lv/run-request)))))
-  (testing "Caller identification shall return a coll with 12 items"
-    (is (= 12 (count (cid/run-request))))))
+  (testing "Phone search shall return a coll with 12 items"
+    (is (= 12 (count (ps/run-request))))))
